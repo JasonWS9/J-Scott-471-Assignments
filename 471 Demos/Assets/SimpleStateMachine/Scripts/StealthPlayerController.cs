a9ae9dae2a0babb3ae89ba79f6ec1c7e9ba4a208
JasonWS9 <JasonWScott10@gmail.com>
2025-02-13 20:51:36 -0500

    asdasddsa

--- 471 Demos/Assets/SimpleStateMachine/Scripts/StealthPlayerController.cs
+++ 471 Demos/Assets/SimpleStateMachine/Scripts/StealthPlayerController.cs
@@ -1,17 +1,16 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class StealthPlayerController : MonoBehaviour
 {
 
 
     Vector2 movement;
-    Vector2 mouseMovement;
     CharacterController controller;
     [SerializeField] float playerSpeed = 10f;
-    [SerializeField] float mouseSensitivity = 100f;
-    [SerializeField] GameObject cam;
-    float cameraUpRotation = 0;
+
 
     private void Start()
     {
@@ -21,16 +20,6 @@ public class StealthPlayerController : MonoBehaviour
     void Update()
     {
 
-        float LookX = mouseMovement.x * Time.deltaTime * mouseSensitivity;
-        float LookY = mouseMovement.y * Time.deltaTime * mouseSensitivity;
-
-        cameraUpRotation -= LookY;
-
-        cameraUpRotation = Mathf.Clamp(cameraUpRotation, -90, 90);
-
-        cam.transform.localRotation = Quaternion.Euler(cameraUpRotation, 0, 0);
-
-        transform.Rotate(Vector3.up * LookX);
 
         float MoveX = movement.x;
         float MoveZ = movement.y;
@@ -54,13 +43,34 @@ public class StealthPlayerController : MonoBehaviour
         movement = moveVal.Get<Vector2>();
     }
 
-    void OnLook(InputValue lookVal)
+    /*
+    private void OnCollisionEnter(Collision collision)
     {
-        mouseMovement = lookVal.Get<Vector2>();
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            GameOver();
+        }
+    }
+
+    */
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EnemyController enemy = collision.gameObject.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            GameOver();
+        }
     }
 
 
- 
+    private void GameOver()
+    {
+        Debug.Log("Game Over");
+        SceneManager.LoadScene("SimpleStateMachine");
+    }
+
+
 
 
 
